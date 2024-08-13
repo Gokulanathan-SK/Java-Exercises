4aa906f53ef9ee26dcc8f7936f5c54389e93bcb1
Gokulanathan-SK <gokulanathansk1734@gmail.com>
2024-08-13 20:32:22 +0530

    Fix Redundancy in Declaration

--- BasicsPart_1/Problem020.java
+++ BasicsPart_1/Problem020.java
@@ -14,7 +14,7 @@ public class Problem020 {
         System.out.print("Input a Decimal Number: ");
         int decimalNumber = get.nextInt();
 
-        int remainder = 0;
+        int remainder;
         String hexNumber = "";
         char[] hex = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
